76762cbdc3780a041f685931e24f36a2d2a10535
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2020-02-05 20:14:43 -0800

    updating

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,7 +7,7 @@ var $noteList = $(".list-container .list-group");
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
 
-// A function for getting all notes from the datab
+// A function for getting all notes from the databa
 var getNotes = function() {
   return $.ajax({
     url: "/api/notes",
